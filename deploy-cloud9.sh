9d7231b486714dca9758de67b3adc88e5effacef
fav1c0n <44481988+fav1c0n@users.noreply.github.com>
2022-02-28 12:36:01 +0100

    Update deploy-cloud9.sh

--- deploy-cloud9.sh
+++ deploy-cloud9.sh
@@ -1,3 +1,5 @@
+set -x
+
 # Instrucciones para despliegue sin contenerizacion
 
 # Instrucciones de depliegue de hasher
@@ -12,3 +14,32 @@ ruby ./hasher/hasher.rb &
 
 # Instrucciones depliegue redis
 sudo yum install -y redis
+
+sudo tee -a /etc/hosts 0<<FINAL
+#redis
+127.0.0.1 redis
+FINAL
+
+sudo systemctl start redis.service
+
+# Instrucciones para desplegar rng
+pip install Flask
+
+sudo tee -a /etc/hosts 0<<FINAL
+#rng
+127.0.0.1 rng
+FINAL
+
+python ./rng/rng.py &
+
+# Instrucciones para desplegar el worker
+pip install redis request
+
+python ./worker/worker.py &
+
+# Instrucciones para instalar webUI
+npm i express redis@3.1.2
+
+cd ./webui && node webui.js &
+
+set +x
