ddc56cc17925b60fbb560f0a500e54b5cf5d78b1
ele-igle <100281645+ele-igle@users.noreply.github.com>
2022-03-01 11:36:59 +0100

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,19 +1,19 @@
-    9  git clone https://github.com/ele-igle/dockercoins-1.git
-   10  cd dockercoins-1/
-   11  docker network create hasher
-   12  docker network create redis
-   13  docker network create rng
-   23  docker build -t ruby:sinatra-thin ./hasher/
-    docker run -d --rm --name hasher --network hasher --entrypoint ruby -u nobody -v $PWD/hasher/hasher.rb:/hasher.rb ruby:sinatra-thin hasher.rb
-   26  docker ps
-   27  docker logs hasher
-   28  docker top hasher
-   docker run -d --rm --name redis --network redis --entrypoint docker-entrypoint.sh -u redis -v redis-data:/data/ redis:latest redis-server
-   31  docker ps
-   32  docker logs redis
-   33  docker top redis
-   docker build -t python:Flask ./rng/
+git clone https://github.com/ele-igle/dockercoins-1.git
+cd dockercoins-1/
+docker network create hasher
+docker network create redis
+docker network create rng
+docker build -t ruby:sinatra-thin ./hasher/
+docker run -d --rm --name hasher --network hasher --entrypoint ruby -u nobody -v $PWD/hasher/hasher.rb:/hasher.rb ruby:sinatra-thin hasher.rb
+docker ps
+docker logs hasher
+docker top hasher
+docker run -d --rm --name redis --network redis --entrypoint docker-entrypoint.sh -u redis -v redis-data:/data/ redis:latest redis-server
+docker ps
+docker logs redis
+docker top redis
+docker build -t python:Flask ./rng/
 docker run -d --rm --name rng --network rng --entrypoint python -u nobody -v $PWD/rng/rng.py:/rng.py python:Flask rng.py
-   41  docker ps
-   docker logs rng
-   44  docker top rng
+docker ps
+docker logs rng
+docker top rng
